2d1501efa292f9a873bdb62852fc91a60cffcecf
Gopal Krishnan <gk.krishnan92@gmail.com>
2015-07-05 16:05:43 -0400

    making list view functional - broken currently

--- app/src/main/java/com/example/eobardallen/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/example/eobardallen/sunshine/MainActivityFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -22,7 +24,7 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         String[] data = {
                 "Mon 6/23 - Sunny - 31/7",
                 "Tue 6/24 - Foggy - 21/8",
@@ -34,7 +36,10 @@ public class MainActivityFragment extends Fragment {
         };
 
         List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
+
+        ListView weatherDetails = (ListView)rootView.findViewById(R.id.list_item_forecast_textview);
+        weatherDetails.setAdapter(dataAdapter);
 
         return rootView;
     }
