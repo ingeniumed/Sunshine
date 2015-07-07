122e4e62b331f6e454cc3408c598712ae06cd287
Gopal Krishnan <gk.krishnan92@gmail.com>
2015-07-06 21:34:22 -0400

    Refresh based on zip  using async task

--- app/src/main/java/com/example/eobardallen/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/example/eobardallen/sunshine/MainActivityFragment.java
@@ -1,9 +1,14 @@
 package com.example.eobardallen.sunshine;
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -24,9 +29,15 @@ import java.util.List;
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
-    String fragment_tag = "MainActivityFragment";
+    String LOG_TAG = MainActivityFragment.class.getSimpleName();
 
     public MainActivityFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -48,56 +59,110 @@ public class MainActivityFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         ListView weatherDetails = (ListView)rootView.findViewById(R.id.listview_forecast);
         weatherDetails.setAdapter(dataAdapter);
-
-        HttpURLConnection urlConnection = null;
-        BufferedReader reader = null;
-
-        String forecastJsonStr = null;
-
-        try{
-            URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("GET");
-            urlConnection.connect();
-
-            InputStream inputStream = urlConnection.getInputStream();
-            StringBuffer buffer = new StringBuffer();
-            if (inputStream == null)
-            {
-                return null;
-            }
-            reader = new BufferedReader(new InputStreamReader(inputStream));
-
-            String line = "";
-            while ((line = reader.readLine()) != null)
-            {
-                buffer.append(line + "\n");
-            }
-
-            if (buffer.length() == 0)
-            {
-                return null;
-            }
-
-            forecastJsonStr = buffer.toString();
-        } catch (IOException e) {
-            Log.e(fragment_tag, "Error", e);
-            return null;
-        } finally{
-            if (urlConnection != null)
-            {
-                urlConnection.disconnect();
-            }
-            if (reader != null)
-            {
-                try{
-                    reader.close();
-                } catch (final IOException e){
-                    Log.e(fragment_tag, "Error closing stream", e);
-                }
-            }
-        }
-
         return rootView;
     }
+
+    public class FetchWeatherTask extends AsyncTask<String, Void, Void>
+    {
+        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
+
+        @Override
+        protected Void doInBackground(String... params){
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            String forecastJsonStr = null;
+
+            String format = "json";
+            String units = "metric";
+            int numOfDays = 7;
+
+            try{
+                final String FORECAST_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?";
+                final String QUERY_PARAM = "q";
+                final String FORMAT_PARAM = "mode";
+                final String UNITS_PARAM = "units";
+                final String DAYS_PARAM = "cnt";
+
+                Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                        .appendQueryParameter(QUERY_PARAM, params[0])
+                        .appendQueryParameter(FORMAT_PARAM, format)
+                        .appendQueryParameter(UNITS_PARAM, units)
+                        .appendQueryParameter(DAYS_PARAM, Integer.toString(numOfDays))
+                        .build();
+
+                URL url = new URL(builtUri.toString());
+
+                Log.v(LOG_TAG, "Built URI " + builtUri.toString());
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null)
+                {
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line = "";
+                while ((line = reader.readLine()) != null)
+                {
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0)
+                {
+                    return null;
+                }
+
+                forecastJsonStr = buffer.toString();
+                Log.v(LOG_TAG, "Forecast JSON String: " +forecastJsonStr);
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error", e);
+                return null;
+            } finally{
+                if (urlConnection != null)
+                {
+                    urlConnection.disconnect();
+                }
+                if (reader != null)
+                {
+                    try{
+                        reader.close();
+                    } catch (final IOException e){
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        inflater.inflate(R.menu.menu_forecastfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_refresh) {
+            FetchWeatherTask FWT = new FetchWeatherTask();
+            FWT.execute("94043");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
