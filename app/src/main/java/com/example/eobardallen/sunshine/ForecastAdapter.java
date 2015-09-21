9d2f37df79091393d81d534539a45330c6552c9b
GOPAL KRISHNAN <gk.krishnan92@gmail.com>
2015-09-20 20:37:40 -0400

    Beginning two pane layout

--- app/src/main/java/com/example/eobardallen/sunshine/ForecastAdapter.java
+++ app/src/main/java/com/example/eobardallen/sunshine/ForecastAdapter.java
@@ -71,8 +71,21 @@ public class ForecastAdapter extends CursorAdapter {
         // read weather icon ID from cursor
         int weatherID = cursor.getInt(MainActivityFragment.COL_WEATHER_ID);
 
-        // Use placeholder image for now
-        viewHolder.iconView.setImageResource(R.drawable.ic_launcher);
+        int viewType = getItemViewType(cursor.getPosition());
+        switch (viewType) {
+            case VIEW_TYPE_TODAY: {
+                // Get weather icon
+                viewHolder.iconView.setImageResource(Utility.getArtResourceForWeatherCondition(
+                        cursor.getInt(MainActivityFragment.COL_WEATHER_CONDITION_ID)));
+                break;
+            }
+            case VIEW_TYPE_FUTURE_DAY: {
+                // Get weather icon
+                viewHolder.iconView.setImageResource(Utility.getIconResourceForWeatherCondition(
+                        cursor.getInt(MainActivityFragment.COL_WEATHER_CONDITION_ID)));
+                break;
+            }
+        }
 
         // Read date from cursor
         long dateInMillis = cursor.getLong(MainActivityFragment.COL_WEATHER_DATE);
